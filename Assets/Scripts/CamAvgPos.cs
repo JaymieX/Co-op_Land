c2fcdd06ac905865dc2dc7f0b33ce36f7a7ffeac
Jaymie Xu <jake9828@gmail.com>
2019-10-17 00:50:02 -0400

    Added camera follow

---
+++ Assets/Scripts/CamAvgPos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamAvgPos : MonoBehaviour
+{
+    public List<GameObject> ObjectsToConsider;
+
+    public GameObject CamGameObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float avgX = 0f;
+        float avgY = 0f;
+
+        foreach (var go in ObjectsToConsider)
+        {
+            avgX += go.transform.position.x;
+            avgY += go.transform.position.y;
+        }
+
+        avgX /= ObjectsToConsider.Count;
+        avgY /= ObjectsToConsider.Count;
+
+        float z = CamGameObject.transform.position.z;
+        CamGameObject.transform.position = new Vector3(avgX, avgY, z);
+    }
+}
